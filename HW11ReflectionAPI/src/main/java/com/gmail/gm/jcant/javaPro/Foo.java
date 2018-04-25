ce2acddff4fedb0baad4cf9b9703b48a37a03a17
jcant <jcant.gm@gmail.com>
2018-04-25 16:23:17 +0300

    HW11 run annotated method - gitignore-test

--- HW11ReflectionAPI/src/main/java/com/gmail/gm/jcant/javaPro/Foo.java
+++ HW11ReflectionAPI/src/main/java/com/gmail/gm/jcant/javaPro/Foo.java
@@ -2,12 +2,12 @@ package com.gmail.gm.jcant.javaPro;
 
 public class Foo {
 
-	@Test(a = 3, b = 2)
+	@Test(a = 3, b = 5)
 	public static int sum(int x, int y) {
 		return x + y;
 	}
 
-	@Test(a = 3, b = 2)
+	@Test(a = 3, b = 5)
 	public static int multiply(int x, int y) {
 		return x * y;
 	}
