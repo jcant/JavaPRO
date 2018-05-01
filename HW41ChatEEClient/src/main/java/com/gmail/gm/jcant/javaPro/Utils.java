cbc4fae4165a83f113c893540371a3c37b4b4b24
jcant <jcant.gm@gmail.com>
2018-05-01 22:39:04 +0300

    HW41 Chat EE

---
+++ HW41ChatEEClient/src/main/java/com/gmail/gm/jcant/javaPro/Utils.java
@@ -0,0 +1,10 @@
+package com.gmail.gm.jcant.javaPro;
+
+public class Utils {
+    private static final String URL = "http://192.168.0.91";
+    private static final int PORT = 8080;
+
+    public static String getURL() {
+        return URL + ":" + PORT;
+    }
+}
---
+++ HW41ChatEEServer/src/com/gmail/gm/jcant/javaPro/GetListServlet.java
@@ -0,0 +1,42 @@
+package com.gmail.gm.jcant.javaPro;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetListServlet extends HttpServlet {
+	
+	private MessageList msgList;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		msgList = MessageList.getInstance();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    	String fromStr = req.getParameter("from");
+		int from = 0;
+		try {
+			from = Integer.parseInt(fromStr);
+		} catch (Exception ex) {
+			System.out.println(ex);
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+		}
+		
+		String json = msgList.toJSON(from);
+		if (json != null) {
+			OutputStream os = resp.getOutputStream();
+            byte[] buf = json.getBytes(StandardCharsets.UTF_8);
+			os.write(buf);
+		} else{
+		}
+	}
+}
